5881394cbcf861df02163a7773de12c6483949ba
neonspectrum <youngskymann@gmail.com>
2018-11-30 02:01:12 +0800

    oops

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,9 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="stylesheet" href="{{ asset('public/css/packages.css') }}?v={{ filemtime(public_path('css/packages.css')) }}">
-  <link rel="stylesheet" href="{{ asset('public/css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}">
-  <link rel="stylesheet" href="{{ asset('public/css/app-responsive.css') }}?v={{ filemtime(public_path('css/app-responsive.css')) }}" media="screen and (max-width:767px)">
+  <link rel="stylesheet" href="{{ asset('css/packages.css') }}?v={{ filemtime(public_path('css/packages.css')) }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}">
+  <link rel="stylesheet" href="{{ asset('css/app-responsive.css') }}?v={{ filemtime(public_path('css/app-responsive.css')) }}" media="screen and (max-width:767px)">
   <title>933 Co-working Manila</title>
 </head>
 <body>
@@ -27,9 +27,9 @@
   @include('navbar')
   @yield('body')
   @include('footer')
-  <script src="{{ asset('public/manifest.js') }}?v={{ filemtime(public_path('manifest.js')) }}"></script>
-  <script src="{{ asset('public/js/vendor.js') }}?v={{ filemtime(public_path('js/vendor.js')) }}"></script>
-  <script src="{{ asset('public/js/packages.js') }}?v={{ filemtime(public_path('js/packages.js')) }}"></script>
-  <script src="{{ asset('public/js/app.js') }}?v={{ filemtime(public_path('js/app.js')) }}"></script>
+  <script src="{{ asset('manifest.js') }}?v={{ filemtime(public_path('manifest.js')) }}"></script>
+  <script src="{{ asset('js/vendor.js') }}?v={{ filemtime(public_path('js/vendor.js')) }}"></script>
+  <script src="{{ asset('js/packages.js') }}?v={{ filemtime(public_path('js/packages.js')) }}"></script>
+  <script src="{{ asset('js/app.js') }}?v={{ filemtime(public_path('js/app.js')) }}"></script>
 </body>
 </html>
