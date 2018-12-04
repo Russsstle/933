06584340170754eff417eff3e8f84b6451b0434a
neonspectrum <youngskymann@gmail.com>
2018-12-04 21:47:50 +0800

    use mix versioning

--- resources/views/cpanel/layouts/app.blade.php
+++ resources/views/cpanel/layouts/app.blade.php
@@ -5,27 +5,27 @@
   <meta charset="UTF-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('apple-icon-57x57.png') }}">
-  <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('apple-icon-60x60.png') }}">
-  <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('apple-icon-72x72.png') }}">
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('apple-icon-76x76.png') }}">
-  <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('apple-icon-114x114.png') }}">
-  <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('apple-icon-120x120.png') }}">
-  <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('apple-icon-144x144.png') }}">
-  <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('apple-icon-152x152.png') }}">
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-icon-180x180.png') }}">
-  <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('android-icon-192x192.png') }}">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-  <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon-96x96.png') }}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-  <link rel="manifest" href="{{ asset('manifest.json') }}">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="{{ asset('ms-icon-144x144.png') }}">
+  <meta name="msapplication-TileImage" content="{{ asset('icon/ms-icon-144x144.png') }}">
   <meta name="theme-color" content="#ffffff">
-  <link rel="stylesheet" href="{{ asset('css/cpanel/packages.css') }}?v={{ filemtime(public_path('css/cpanel/packages.css')) }}">
-  <link rel="stylesheet" href="{{ asset('css/cpanel/app.css') }}?v={{ filemtime(public_path('css/cpanel/app.css')) }}">
-  <link rel="stylesheet" href="{{ asset('css/cpanel/app-responsive.css') }}?v={{ filemtime(public_path('css/cpanel/app-responsive.css')) }}" media="screen and (max-width:767px)">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('icon/apple-icon-57x57.png') }}">
+  <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('icon/apple-icon-60x60.png') }}">
+  <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('icon/apple-icon-72x72.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('icon/apple-icon-76x76.png') }}">
+  <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('icon/apple-icon-114x114.png') }}">
+  <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('icon/apple-icon-120x120.png') }}">
+  <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('icon/apple-icon-144x144.png') }}">
+  <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('icon/apple-icon-152x152.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icon/apple-icon-180x180.png') }}">
+  <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('icon/android-icon-192x192.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('icon/favicon-96x96.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icon/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('icon/manifest.json') }}">
+  <link rel="stylesheet" href="{{ mix('css/cpanel/packages.css') }}">
+  <link rel="stylesheet" href="{{ mix('css/cpanel/app.css') }}">
+  <link rel="stylesheet" href="{{ mix('css/cpanel/app-responsive.css') }}" media="screen and (max-width:767px)">
   <title>933 Co-working Manila</title>
 </head>
 <body>
@@ -46,7 +46,7 @@
     @yield("body")
   @overwrite
   @include("cpanel.layouts.sidebar")
-  <script src="{{ asset('js/cpanel/packages.js') }}?v={{ filemtime(public_path('js/cpanel/packages.js')) }}"></script>
-  <script src="{{ asset('js/cpanel/app.js') }}?v={{ filemtime(public_path('js/cpanel/app.js')) }}"></script>
+  <script src="{{ mix('js/cpanel/packages.js') }}"></script>
+  <script src="{{ mix('js/cpanel/app.js') }}"></script>
 </body>
 </html>
