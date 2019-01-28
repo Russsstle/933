fe7402da4c326f2b0ad24cd3dcd85ad7be62f9fd
Russsstle <rjohn.sants@yahoo.com>
2019-01-28 17:34:57 +0800

    navbar adjust

--- resources/views/website/layouts/navbar.blade.php
+++ resources/views/website/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="nav-wrapper">
     <a href="{{ url('/') }}" class="brand-logo"><img src="{{ asset('img/logo-new.png') }}"></a>
     <a href="javascript:void(0)"  data-target="mobile-sidebar" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
-    <ul class="right hide-on-med-and-down">
+    <ul class="right hide-on-med-and-down ul-nav">
       <li class="{{ active('/') }}"><a href="{{ url('/') }}">Home</a></li>
       <li class="{{ active('services/*') }}"><a class="dropdown-trigger" href="javascript:void(0)"  data-target='dropdown2'>Branches<i class="material-icons right">arrow_drop_down</i></a></li>
       <li class="{{ active('blog*') }}"><a  href="{{ url('blog') }}">Blog</a></li>
